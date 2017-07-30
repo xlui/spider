b969aa2179d3f8d1681ad7caa2cd45d2513014ab
liuqi <liuqi0315@gmail.com>
2017-07-30 10:12:29 +0800

    modify comment

--- examples/testLock.py
+++ examples/testLock.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # desc: this program is used to test when using multithread to write data into
 # mongodb, the main program need thread lock or not, and the result shows it
-# do not need.
+# does not need.
 import threading
 import pymongo
 import time
@@ -18,7 +18,7 @@ class MyThread(threading.Thread):
     def run(self):
         print("start thread: ", self.count, time.ctime(time.time()))
         thread_function(self.count)
-        print("stop thread: ", self.count)
+        print("stop thread: ", self.count, time.ctime(time.time()))
 
 
 def thread_function(count):
@@ -33,6 +33,7 @@ start_time = time.time()
 client = pymongo.MongoClient('localhost', 27017)
 db = client['test']
 db.drop_collection('testLock')
+# drop exist table
 table = db['testLock']
 
 thread_number = int(input('> please input the number of threads: '))
