90516537aa9ce2e6248e10a9f1a5cfaa865364fa
liuqi <liuqi0315@gmail.com>
2017-08-20 17:36:54 +0800

    test save data to json format and read data from json format

---
+++ Examples/jsonTest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding:utf-8
+# Try to usr json to save and read data
+import json
+
+data = [
+    {'name':'beijing', 'city':'www.beijing.com'},
+    {'name':'shanghai', 'city':'www.shanghai.com'},
+]
+
+with open('test_json.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file)
+
+with open('test_json.json', 'r', encoding='utf-8') as json_file:
+    read_data = json.load(json_file)
+
+print('print read data: ', read_data)
+print('read data type: ', type(read_data))
+# list
+for data in read_data:
+    print('list element type: ', type(data))
+    # dict
+    print('list element', data)
