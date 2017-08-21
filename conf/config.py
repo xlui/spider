b2749f88e2ac6925d930f13c84b799bbbc85fecb
liuqi <liuqi0315@gmail.com>
2017-08-21 17:32:33 +0800

    Reorganize and Add proxy and mongo related variables

--- conf/config.py
+++ conf/config.py
@@ -1,8 +1,20 @@
 # save config variables here!
 
+# url
 root_url = 'http://www.xiaozhu.com'
 
 
+# spider
 headers = {
     'User-Agent': 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)',
 }
+
+proxies = {
+    'http': 'http://223.13.69.86:8118'
+}
+
+
+# mongodb
+database = 'xiaozhu'
+room_url_collection = 'room_url'
+room_data_collection = 'room_data'
