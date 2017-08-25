057c6271ed779304e5d5235a7c860107cbd0ed21
liuqi <liuqi0315@gmail.com>
2017-08-25 17:45:19 +0800

    Reorganize config.py

--- conf/config.py
+++ conf/config.py
@@ -9,10 +9,11 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (compatible; Baiduspider/2.0; +http://www.baidu.com/search/spider.html)',
 }
 
-proxies = {
-    'http': 'http://223.13.69.86:8118'
-}
-
+proxy_ip = ['113.234.110.98']
+proxy_port = ['80']
+proxies = {}
+# proxies = {'http': 'http://{}:{}'.format(proxy_ip.pop(), proxy_port.pop())}
+# print(proxies)
 
 # mongodb
 database = 'xiaozhu'
