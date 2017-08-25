0cbde58546d16e6efc08d2b09e83debbfaaa3d3f
liuqi <liuqi0315@gmail.com>
2017-08-25 17:45:51 +0800

    Change test url

--- Examples/proxy.py
+++ Examples/proxy.py
@@ -7,6 +7,6 @@ sys.path.append('../')
 from conf.config import headers, proxies
 
 
-url = 'http://www.xiaozhu.com/'
+url = 'http://bj.xiaozhu.com/fangzi/6937392816.html'
 response = requests.get(url, headers=headers, timeout=10, proxies=proxies)
 print(response.text)
