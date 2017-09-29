681b05f14be898df6aa19060df27cb1d8b7766ba
liuqi <liuqi0315@gmail.com>
2017-09-29 18:16:48 +0800

    Another way to test multiprocess save data to mongodb

--- Samples/multiProcessingSaveData.py
+++ Samples/multiProcessingSaveData.py
@@ -1,44 +1,47 @@
-#!/usr/bin/env python
-# coding:utf-8
-# 通过 多进程/多线程 向数据库写入数据
-# 多进程可以成功写入，多线程写入失败。
-import threading
+# test using multiprocess to save data into mongodb
+from contextlib import closing
 from multiprocessing import Pool
-import os
-import pymongo
-
-data = {}
-
-# for i in range(5):
-#     data['_id'] = i
-#     data['thread_count'] = i
-#     collection.insert(data)
+from App.mongodb import MongoDB
+from Samples.measure_time import time_func
 
 
-def data_process(count):
-    client = pymongo.MongoClient('localhost', 27017)
-    xiaozhu = client['xiaozhu']
-    # xiaozhu.drop_collection('test')
-    collection = xiaozhu['test']
-
-    # data['_id'] = count
-    data['thread_count'] = count
-    collection.insert(data)
-    client.close()
-
-# threads = []
-# for i in range(5):
-#     thread = threading.Thread(target=data_process, args=(data, i))
-#     thread.start()
-#     threads.append(thread)
-# for thread in threads:
-#     thread.join()
+def run(collection, data):
+    with closing(MongoDB()) as mongodb:
+        mongodb.save(collection, **data)
 
 
-pool_count_total = 24
-pool = Pool(pool_count_total)
-for pool_count in range(pool_count_total):
-    pool.apply_async(data_process, args=(pool_count,))
-pool.close()
-pool.join()
-print('All data of process', os.getpid(), 'has been insert into mongodb')
+@time_func
+def main(collection):
+    pool_count = 16
+    data_count = 10000
+    # result:
+    # process   data_count  time
+    #   1           100         0.7
+    #   1           1000        3.21
+    #   1           10000       46
+    #   4           100         0.41
+    #   4           1000        1.6
+    #   4           10000       30-40
+    #   8           100         0.42
+    #   8           1000        1.53
+    #   8           10000       35
+    #   16          10000       30
+    pool = Pool(pool_count)
+    list_to_be_saved = [{'page': i, 'url': 'https://www.baidu.com/?page={}'.format(i)} for i in range(data_count)]
+
+    [pool.apply_async(run, args=(collection, list_to_be_saved[i])) for i in range(data_count)]
+
+    print('Waiting for all subprocess to done...')
+    pool.close()
+    pool.join()
+    print('All subprocess done.')
+    print('Exit main process.')
+
+
+if __name__ == '__main__':
+    collection = 'test'
+
+    with closing(MongoDB()) as mongodb:
+        mongodb.drop(collection)
+
+    main(collection)
