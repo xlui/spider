d63a3848736c0b7e807bedaa7dc20ef2816fd463
xlui <liuqi0315@gmail.com>
2017-09-30 14:43:23 +0800

    Use two dict to process data

--- App/get_room_data.py
+++ App/get_room_data.py
@@ -20,48 +20,32 @@ class GetRoomData(object):
         :return: room data dict
         """
         web_data = requests.get(self.__url,
-                                headers=headers[randint(0, headers_length)],
-                                proxies=proxies[randint(0, proxies_length)])
+                                headers=headers[randint(0, headers_length - 1)],
+                                proxies=proxies[randint(0, proxies_length - 1)])
         web_data.encoding = 'utf-8'
         soup = BeautifulSoup(web_data.text, 'lxml')
 
-        # todo: find a better way to do this.
-        t_title = soup.select('h4 em')
-        title = None if not t_title else t_title[0].text
-
-        t_address = soup.select('span.pr5')
-        address = None if not t_address else t_address[0].text
-
-        t_price = soup.select('div.day_l span')
-        price = None if not t_price else t_price[0].text
-
-        t_img = soup.select('#curBigImage')
-        img = None if not t_img else t_img[0].get('src')
-
-        t_host_pic = soup.select('div.member_pic a img')
-        host_pic = None if not t_host_pic else t_host_pic[0].get('src')
-
-        t_host_name = soup.select('h6 a.lorder_name')
-        host_name = None if not t_host_name else t_host_name[0].text
-
-        host_gender = 'female' if soup.find_all('.member_ico1') else 'male'
-
-        self.__data = {
-            'title': title,
-            'address': address,
-            'price': price,
-            'img': img,
-            'host_pic': host_pic,
-            'host_name': host_name,
-            'host_gender': host_gender,
+        tmp_json = {
+            'title': soup.select('h4 em'),
+            'address': soup.select('span.pr5'),
+            'price': soup.select('div.day_l span'),
+            'img': soup.select('#curBigImage'),
+            'host_pic': soup.select('div.member_pic a img'),
+            'host_name': soup.select('h6 a.lorder_name'),
+            'host_gender': 'female' if soup.find_all('.member_ico1') else 'male'
         }
 
-        for key, value in self.__data.items():
-            # get rid of spaces and '\n' at the last of value
-            try:
-                self.__data[key] = value.strip()
-            except AttributeError:
-                pass
+        self.__data = {
+            'title': '' if not tmp_json['title'] else tmp_json['title'][0].text,
+            'address': '' if not tmp_json['address'] else tmp_json['address'][0].text,
+            'price': '' if not tmp_json['price'] else tmp_json['price'][0].text,
+            'img': '' if not tmp_json['img'] else tmp_json['img'][0].get('src', ''),
+            'host_pic': '' if not tmp_json['host_pic'] else tmp_json['host_pic'][0].get('src', ''),
+            'host_name': '' if not tmp_json['host_name'] else tmp_json['host_name'][0].text,
+            'host_gender': tmp_json['host_gender'],
+        }
+
+        self.__data = {key: value.strip() for key, value in self.__data.items()}
 
         return self.__data
 
@@ -85,4 +69,6 @@ class GetRoomData(object):
 if __name__ == '__main__':
     url = 'http://bj.xiaozhu.com/fangzi/14936062703.html'
     get_room_data = GetRoomData(url)
-    print(get_room_data.get())
+    data = get_room_data.get()
+    for key, value in data.items():
+        print('{}: {}'.format(key, value))
